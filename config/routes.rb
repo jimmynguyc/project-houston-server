a9fe74da6c87a02a3ed84ab5a9ed125064bff404
minghaochan <cmh114933@gmail.com>
2016-12-09 11:15:01 +0800

    clearance installed with added dashboard homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  constraints Clearance::Constraints::SignedIn.new do
+    root to: 'users#dashboard', as: :admin_root
+  end
+  constraints Clearance::Constraints::SignedOut.new do
+    root to: 'clearance/sessions#new'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
