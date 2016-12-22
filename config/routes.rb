6aecf661083070d47f0a017c4cdf8da3508a4b73
minghaochan <cmh114933@gmail.com>
2016-12-22 14:29:36 +0800

    set timer complete

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
     root to: 'clearance/sessions#new'
   end
   patch 'user/:id/timezone_set' => 'users#timeset', as: :set_time_zone
+  get 'aircond/:id/timer' => 'airconds#timer', as: :set_timer
+  patch 'aircond/:id/timer' => 'airconds#timer_set', as: :timer_set
   resources :devices
   resources :airconds
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
