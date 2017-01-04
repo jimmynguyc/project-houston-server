5f810df3033bff6a9eb0f8cdd0d82a652eeedcf9
minghaochan <cmh114933@gmail.com>
2017-01-04 11:28:49 +0800

    configured output to enable setting of aircond state

--- app/models/aircond.rb
+++ app/models/aircond.rb
@@ -27,35 +27,13 @@ class Aircond < ApplicationRecord
 		return {status:response.body.strip}	
 	end
 
-	def send_signal(status:) #,mode:,temperature:,fan_speed:
+	def send_signal(command)
 		raspi = self.device
 		path = 'http://'+ raspi.url + "/state.py"
-
-		if validate_AC_controls(status:status)  #future versions can extend more arguments v1
-			# create_lircd_conf_file(state[:signal] )
-			# params = {access_token:raspi.access_token, file: File.new('lircd.conf')}  #sends the entire text for the conf file
-      params = {access_token:raspi.access_token, status:'OFF'}
-
-			response = Unirest.post(path,parameters:params)
-		else
-			return "Invalid command signal"
-		end
+    params = {access_token:raspi.access_token, command:command}
+    byebug
+		response = Unirest.post(path,parameters:params)
 	end
 
-	private
-
-	def validate_AC_controls(status:)
-		#ENSURE no invalid commands sent
-		state = nil
-		state = {signal:InfraredSignal.find_by_command(status).ir_signal_in_conf} if InfraredSignal.pluck(:command).include? status	
-		return state
-	end
-
-#Temporary removal: Current version does not require sending of lircd file
-	# def create_lircd_conf_file(signal)
-	# 		lircd_conf = 'begin remote\n\n   name  Daikin\n   flags RAW_CODES\n   eps            30\n   aeps          100\n\n   frequency    38000\n\n       begin raw_codes\n           ' + signal + '\n       end raw_codes\n\nend remote'
-	# 		%x'rm -rf lircd.conf'
-	# 		%x'echo "#{lircd_conf}">> lircd.conf'		
-	# end
 end
 
