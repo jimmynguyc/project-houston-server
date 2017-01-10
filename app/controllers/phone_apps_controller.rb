919a21942b26f80556a46656c1df3a58830c9586
minghaochan <cmh114933@gmail.com>
2017-01-10 11:08:55 +0800

    basic authorization for phone app set up, pending delivery of app_token

--- app/controllers/phone_apps_controller.rb
+++ app/controllers/phone_apps_controller.rb
@@ -1,9 +1,31 @@
 class PhoneAppsController < ApplicationController
+	scope :filter_by_status, -> (status){where(status:status)}
+	include ApplicationHelper
+	def create
+		PhoneApp.create(phone_app_params)
+	end
+
+	def index
+		@phone_apps = PhoneApp.filter_by_status(params[:status])
+		@phone_apps = PhoneApp.all if params[:status] == nil
+	end
+
 	def app_get_token
 		
 	end
 
-	def app_set_token
+	def app_approve_token
+		@phone_app = PhoneApp.find(params[:id])
+		if is_admin?
+			@phone_app.update(access_token:generate_security_token)
+		else
+			flash[:warning] = "You do not have authority to execute this action"
+			redirect_to root_path
+		end
 	end
 
+	private
+	def phone_app_params
+		params.require(:phone_app).permit('user_name')
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170106040816) do
+ActiveRecord::Schema.define(version: 20170110024204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20170106040816) do
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
     t.integer  "status",       default: 0
+    t.string   "user_name"
     t.index ["user_id"], name: "index_phone_apps_on_user_id", using: :btree
   end
 
