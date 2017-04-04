46f0824b5841d5ab478803e87166da6fa0638458
mhchan <cmh114933@gmail.com>
2017-04-04 17:25:37 +0800

    added pagination for log

--- aircond_rails_app/app/controllers/versions_controller.rb
+++ aircond_rails_app/app/controllers/versions_controller.rb
@@ -1,9 +1,10 @@
 class VersionsController < ApplicationController
   before_action :require_login
   def index
+
     if is_admin?
       @aircond = Aircond.find(params[:aircond_id])
-      @versions = @aircond.versions
+      @versions = @aircond.versions.paginate(:page => params[:page],:pera_page => 30).order('created_at DESC')
     else
       flash[:warning] = "You are not authorized."
       redirect_to root_path
