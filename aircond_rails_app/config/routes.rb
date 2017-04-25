0ebb0df14204cf58c32ad57c2f5bae54014d5743
cmh114933 <cmh114933@gmail.com>
2017-04-25 17:18:23 +0800

    remove sign_up path

--- aircond_rails_app/config/routes.rb
+++ aircond_rails_app/config/routes.rb
@@ -14,7 +14,6 @@ mount Sidekiq::Web => '/sidekiq'
 
   get "/sign_in" => "sessions#new", as: "sign_in"
   delete "/sign_out" => "sessions#destroy", as: "sign_out"
-  get "/sign_up" => "users#new", as: "sign_up"
   
   constraints Clearance::Constraints::SignedIn.new do
     root to: 'users#dashboard', as: :admin_root
