d3c73fc98a0b8dbbe2d448df5a462d65735d30f2
cmh114933 <cmh114933@gmail.com>
2017-05-02 16:23:19 +0800

    fixed minor bug in ac status monitor worker

--- aircond_rails_app/app/workers/ac_status_monitor_worker.rb
+++ aircond_rails_app/app/workers/ac_status_monitor_worker.rb
@@ -6,7 +6,7 @@ class AcStatusMonitorWorker
         
     Aircond.all.each do |aircond|
       aircond.check_device_status ? pi_state = "ENABLED" : pi_state = "DISABLED"
-      firebase.update('/airconds/'+ aircond.id.to_s, {pi_status: state})
+      firebase.update('/airconds/'+ aircond.id.to_s, {pi_status: pi_state})
     end
   end
 end
