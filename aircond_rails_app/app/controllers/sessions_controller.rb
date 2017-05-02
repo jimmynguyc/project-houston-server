16668e2e3e057a38568761fcbcc7f9a5df918e9a
cmh114933 <cmh114933@gmail.com>
2017-05-02 16:59:43 +0800

    removed test message

--- aircond_rails_app/app/controllers/sessions_controller.rb
+++ aircond_rails_app/app/controllers/sessions_controller.rb
@@ -2,8 +2,6 @@ class SessionsController < Clearance::SessionsController
 
   def create_from_omniauth
     auth_hash = request.env["omniauth.auth"]
-    p auth_hash
-    p auth_hash["info"]["admin"]
     authentication = Authentication.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"]) || Authentication.create_with_omniauth(auth_hash)
     User.oauth_sign_up?(true)
     if authentication.user
