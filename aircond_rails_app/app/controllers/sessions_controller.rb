1a646b0f49e2d8132d373e2f265960cc9c7bae43
cmh114933 <cmh114933@gmail.com>
2017-05-02 16:29:08 +0800

    configured for oauth_admin authenticatin

--- aircond_rails_app/app/controllers/sessions_controller.rb
+++ aircond_rails_app/app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < Clearance::SessionsController
 
   def create_from_omniauth
     auth_hash = request.env["omniauth.auth"]
+    p auth_hash["info"]
     authentication = Authentication.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"]) || Authentication.create_with_omniauth(auth_hash)
     User.oauth_sign_up?(true)
     if authentication.user
