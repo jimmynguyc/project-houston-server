69c1100d0e26d47ba4be16cda305f1739724edad
Ming Xiang Chan <mingxiangchan@gmail.com>
2017-05-03 18:07:12 +0800

    fixed only 1 id being referenced for each power toggle

--- aircond_rails_app/app/assets/javascripts/components/aircon_card/card_header.jsx
+++ aircond_rails_app/app/assets/javascripts/components/aircon_card/card_header.jsx
@@ -9,23 +9,25 @@ class CardHeader extends React.Component {
   }
 
   render() {
+    const switchId = "onoffswitch-" + this.props.title
+
     return (
       <div className="pmd-card-media card-header">
         <div className="media-body card-header-title">
           <h2 className="pmd-card-title-text">{this.props.title}</h2>
           <span className="pmd-card-subtitle-text">AG-1-111</span>
         </div>
-        
+
         <div className="media-right media-middle">
           <div className="onoffswitch card-power-toggle">
             <input
               type="checkbox"
-              id="myonoffswitch"
+              id={ switchId }
               className="onoffswitch-checkbox"
               name="onoffswitch"
               checked={this.props.power}
               onChange={this.togglePower}/>
-            <label htmlFor="myonoffswitch" className="onoffswitch-label">
+            <label htmlFor={switchId} className="onoffswitch-label">
               <span className="onoffswitch-inner"></span>
               <span className="onoffswitch-switch"></span>
             </label>
